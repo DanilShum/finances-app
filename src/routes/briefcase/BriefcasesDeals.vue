c8e05f8be45a811167baff9a7ff843a74372248c
daniilsumilkin <danil.shumilkin.96@gmail.com>
2021-07-10 16:00:15 +0300

    update

---
+++ src/routes/briefcase/BriefcasesDeals.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="briefcases-deals">
+    <base-table
+      :row="deals"
+      :headers="tableHeaders"
+      show-actions
+      editable
+      @add="showPopupDeals = true"
+      @remove="removeItemDeal"
+      :loading="loading"
+    />
+    <popup-deal
+      v-if="showPopupDeals"
+      @close="showPopupDeals = false"
+      @create="addDeal"
+    />
+  </div>
+</template>
+
+<script>
+import BaseTable from "@/components/BaseTable";
+import { mapActions, mapState } from "vuex";
+import PopupDeal from "@/routes/briefcase/PopupDeal";
+import { db } from "@/main";
+import firebase from "firebase/app";
+
+export default {
+  name: "BriefcasesDeals",
+  components: { PopupDeal, BaseTable },
+  props: {},
+  data: () => ({
+    showPopupDeals: false,
+    tableHeaders: [
+      {
+        text: "Название",
+        align: "start",
+        value: "name",
+      },
+      { text: "Тикер", value: "ticker" },
+      { text: "Операция", value: "operation" },
+      { text: "Дата", value: "date" },
+      { text: "Кол-во", value: "count" },
+      { text: "Цена", value: "price" },
+      { text: "Сумма", value: "sum" },
+      {
+        text: "",
+        value: "actions",
+        class: "base-table_action-cell",
+        sortable: false,
+      },
+    ],
+    assets: [],
+    deals: [],
+  }),
+  computed: {
+    ...mapState({
+      // deals: (state) => state.deals.list,
+      // assets: (state) => state.assets.list,
+      loading: (state) => state.deals.loading,
+    }),
+  },
+  methods: {
+    ...mapActions({
+      createDeal: "deals/create",
+      createAsset: "assets/create",
+      updateAsset: "assets/updateItem",
+      removeItemDeal: "deals/removeItem",
+    }),
+    async addDeal(deal) {
+      this.createDeal(deal);
+      const assetIndex = this.assets.findIndex(
+        (asset) => asset.ticker === deal.ticker
+      );
+
+      // TODO: move to store
+      const payload = {
+        name: deal.name,
+        count: Number(deal.count),
+        middlePrice: Number(deal.price),
+        currentPrice: 24,
+        cost: Number(deal.count) * 24,
+        profit: 1,
+        profitability: 16,
+        percent: 4,
+        ticker: deal.ticker,
+      };
+
+      if (assetIndex < 0) {
+        this.createAsset(payload);
+      } else {
+        const asset = this.assets[assetIndex];
+        console.log(this.assets);
+
+        // TODO: _.mean
+        const middlePrice = this.middlePrice(
+          payload.middlePrice,
+          asset.middlePrice,
+          payload.count,
+          asset.count
+        );
+        const updatedAsset = {
+          name: payload.name,
+          count: payload.count + asset.count,
+          middlePrice,
+          currentPrice: 24,
+          cost: (payload.count + asset.count) * 24,
+          profit: 1,
+          profitability: 45,
+          percent: 7,
+          ticker: payload.ticker,
+        };
+        this.updateAsset({ item: updatedAsset, index: assetIndex });
+      }
+    },
+    middlePrice(val1, val2, count1, count2) {
+      return (val1 * count1 + val2 * count2) / (count1 + count2);
+    },
+  },
+  firestore() {
+    return {
+      deals:
+        db
+          .collection("deals")
+          .where("userId", "==", firebase.auth().currentUser.uid) || [],
+      assets:
+        db
+          .collection("assets")
+          .where("userId", "==", firebase.auth().currentUser.uid) || [],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.briefcases-deals {
+  width: 100%;
+  .base-table {
+    height: 100%;
+  }
+}
+</style>
